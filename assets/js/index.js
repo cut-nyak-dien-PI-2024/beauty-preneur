7bbb383e917d38c62ff8fd59d4622dfd50ecb400
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2024-09-14 23:28:14 +0700

    scroll active link

--- assets/js/index.js
+++ assets/js/index.js
@@ -41,4 +41,27 @@ var swiper = new Swiper(".popular__container", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
   },
-});~
+});
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link'); // Change 'add' to 'remove'
+        }
+    });
+}
+
+window.addEventListener('scroll', scrollActive);
+
+~
