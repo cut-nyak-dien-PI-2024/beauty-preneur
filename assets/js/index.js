755cbad108b708b5329be7b8d1262d58fb473fd0
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2024-09-15 02:19:25 +0700

    delete function navbar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,4 @@
-let getBody = document.getElementsByTagName("body")[0];
 
-//dom change, close nav
-document.addEventListener("DOMContentLoaded", () => {
-  let getNavMobile = document.getElementById("container__header__mobile");
-  getNavMobile.style.display = "none";
-});
-
-let getMenuMobile = document.getElementById("btn-menu");
-let getCancel = document.getElementById("btn-cancel");
-
-getMenuMobile.addEventListener("click", () => {
-  let getNavMobile = document.getElementById("container__header__mobile");
-  getNavMobile.style.display = "flex";
-});
-
-getCancel.addEventListener("click", () => {
-  let getNavMobile = document.getElementById("container__header__mobile");
-  getNavMobile.style.display = "none";
-});
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader() {
